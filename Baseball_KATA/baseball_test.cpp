973ba0e2f35f78beb8a73656e25f7260a92aef9d
epicpoem <epicpoem@gmail.com>
2026-05-28 13:14:36 +0900

    [Refactoring] 20 세글자가 아닐때 Exce

--- Baseball_KATA/baseball_test.cpp
+++ Baseball_KATA/baseball_test.cpp
@@ -1,9 +1,11 @@
 #include "gmock/gmock.h"
 #include "baseball.cpp"
 
-TEST(BaseballGame, TryGameTest) {
-	EXPECT_EQ(1, 1);
+TEST(BaseballGame, ThrowExceptionWhenInputLengthIsUnmached) {
+	Baseball game;
+	EXPECT_THROW(game.guess(string("12")), length_error);
 }
+
 
 int main() {
 	::testing::InitGoogleMock();
